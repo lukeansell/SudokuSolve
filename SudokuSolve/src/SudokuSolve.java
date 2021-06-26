7d64fb13b5709304e28d33054d5139a0ab39c4a3
Luke <dukeluke140@gmail.com>
2021-06-26 19:48:23 +0200

    further developed createBoard(). Added overloaded methods for using a given board. Added checkSolveable(board)

--- SudokuSolve/src/SudokuSolve.java
+++ SudokuSolve/src/SudokuSolve.java
@@ -4,7 +4,9 @@ import java.awt.Font;
 public class SudokuSolve {
 
 	private static int[][] board = new int[9][9];
+	private static int[][] boardTest = new int[9][9];
 	private static boolean[][] empty = new boolean[9][9];
+	private static boolean[][] emptyTest = new boolean[9][9];
 	private static final int SIZE = 180;
 	private static int[][][] boardPos = new int[9][9][2];
 	private static final int DISPLAYDELAY = 0;
@@ -17,12 +19,13 @@ public class SudokuSolve {
 		StdDraw.setFont(new Font("Arial", Font.BOLD, 24));
 		populateBoardPos();
 		drawGrid();
-		// String filename = "sudoku3.txt";
-		// populateBoard(filename);
-		Stopwatch sw = new Stopwatch();
 
-		createBoard();
-		boardPrintNice();
+		String filename = "sudoku1.txt";
+		populateBoard(filename, boardTest, emptyTest);
+		Stopwatch sw = new Stopwatch();
+		StdOut.println(checkSolvable(boardTest));
+		// createBoard();
+		boardPrintNice(boardTest);
 		visBoard();
 		// if (textMode) {
 		// boardPrintNice();
@@ -117,6 +120,17 @@ public class SudokuSolve {
 		return validBoard();
 	}
 
+	public static boolean complete(int boardS[][]) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (boardS[i][j] == 0) {
+					return false;
+				}
+			}
+		}
+		return validBoard(boardS);
+	}
+
 	public static void solveVis() {
 		int nRow = 9;
 		int nCol = 9;
@@ -172,10 +186,72 @@ public class SudokuSolve {
 			}
 			board[nRow][nCol] = 0;
 		}
+
+	}
+
+	public static void solve(int boardS[][]) {
+		int nRow = 9;
+		int nCol = 9;
+		for (int i = 0; i < 9 && nRow == 9; i++) {
+			for (int j = 0; j < 9 && nRow == 9; j++) {
+				if (boardS[i][j] == 0) {
+					nRow = i;
+					nCol = j;
+					break;
+				}
+			}
+		}
+		if (nCol == 9) {
+			return;
+		}
+		for (int i = 1; i < 10; i++) {
+			boardS[nRow][nCol] = i;
+			if (validBoard(boardS)) {
+				solve(boardS);
+			}
+			if (complete(boardS)) {
+				return;
+			}
+			boardS[nRow][nCol] = 0;
+		}
+
+	}
+
+	public static boolean checkSolvable(int boardT[][]) {
+		int copy[][] = boardCopy(boardT);
+		// int copyOg[][] = boardCopy(boardT);
+		solve(copy);
+		// boardPrintNice(boardT);
+		// boardPrintNice(copy);
+		// boardPrintNice(copyOg);
+		// return !sameBoard(copyOg, copy);
+		return complete(copy);
+	}
+
+	public static boolean sameBoard(int boardT[][], int boardS[][]) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (boardT[i][j] != boardS[i][j])
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public static int[][] boardCopy(int boardT[][]) {
+		int[][] copy = new int[9][9];
+		for (int i = 0; i < 9; i++)
+			for (int j = 0; j < 9; j++)
+				copy[i][j] = boardT[i][j];
+		return copy;
 	}
 
 	public static boolean validBoard() {
 		return validRowsCols() && validBlocks();
+	}
+
+	public static boolean validBoard(int boardT[][]) {
+		return validRowsCols(boardT) && validBlocks(boardT);
 	}
 
 	public static boolean validBlocks() {
@@ -189,11 +265,38 @@ public class SudokuSolve {
 		return true;
 	}
 
+	public static boolean validBlocks(int boardT[][]) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (!validBlock(i * 3, j * 3, boardT)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 	public static boolean validBlock(int sRow, int sCol) {
 		boolean ls[] = new boolean[9];
 		for (int i = sRow; i < sRow + 3; i++) {
 			for (int j = sCol; j < sCol + 3; j++) {
 				int num = board[i][j];
+				if (num != 0) {
+					if (ls[num - 1]) {
+						return false;
+					}
+					ls[num - 1] = true;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static boolean validBlock(int sRow, int sCol, int boardT[][]) {
+		boolean ls[] = new boolean[9];
+		for (int i = sRow; i < sRow + 3; i++) {
+			for (int j = sCol; j < sCol + 3; j++) {
+				int num = boardT[i][j];
 				if (num != 0) {
 					if (ls[num - 1]) {
 						return false;
@@ -224,12 +327,41 @@ public class SudokuSolve {
 		return true;
 	}
 
+	public static boolean validRowsCols(int boardT[][]) {
+		for (int i = 0; i < 9; i++) {
+			boolean[] numsRow = new boolean[10];
+			boolean[] numsCol = new boolean[10];
+			for (int j = 0; j < 9; j++) {
+				int numR = boardT[i][j];
+				if (numsRow[numR])
+					return false;
+
+				numsRow[numR] = numR != 0;
+				int numC = boardT[j][i];
+				if (numsCol[numC])
+					return false;
+				numsCol[numC] = numC != 0;
+			}
+		}
+		return true;
+	}
+
 	public static void populateBoard(String filename) {
 		In in = new In(filename);
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				board[i][j] = in.readInt();
 				empty[i][j] = board[i][j] == 0;
+			}
+		}
+	}
+
+	public static void populateBoard(String filename, int boardT[][], boolean emptyT[][]) {
+		In in = new In(filename);
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				boardT[i][j] = in.readInt();
+				emptyT[i][j] = boardT[i][j] == 0;
 			}
 		}
 	}
@@ -263,12 +395,30 @@ public class SudokuSolve {
 		StdOut.print(str);
 	}
 
+	public static void boardPrintNice(int boardT[][]) {
+		String str = "";
+		for (int i = 0; i < 9; i++) {
+			if (i % 3 == 0) {
+				str += "-------------------------\n";
+			}
+			for (int j = 0; j < 9; j++) {
+				if (j % 3 == 0) {
+					str += "| ";
+				}
+				str += boardT[i][j] + " ";
+			}
+			str += "|\n";
+		}
+		str += "-------------------------\n";
+		StdOut.print(str);
+	}
+
 	public static void createBoard() {
 		boolean exit = false;
 		int i = 0;
 		int j = 0;
 		while (!exit) {
-			StdOut.println("i: " + i + " j: " + j);
+			// StdOut.println("i: " + i + " j: " + j);
 			highlightSquare(i, j);
 			StdDraw.show();
 			while (!StdDraw.hasNextKeyTyped() && !exit) {
@@ -278,16 +428,16 @@ public class SudokuSolve {
 				char ch = StdDraw.nextKeyTyped();
 				switch (ch) {
 					case 'w':
-						i--;
+						i = (i == 0) ? 0 : i - 1;
 						break;
 					case 's':
-						i++;
+						i = (i == 8) ? 8 : i + 1;
 						break;
 					case 'a':
-						j--;
+						j = (j == 0) ? 0 : j - 1;
 						break;
 					case 'd':
-						j++;
+						j = j == 8 ? 8 : j + 1;
 						break;
 					default:
 						String in = "" + ch;
@@ -296,22 +446,22 @@ public class SudokuSolve {
 							board[i][j] = num;
 							// StdOut.println(board[i][j]);
 							// boardPrint();
-							visBoard();
-							i = i + j / 9;
-							j = (j + 9) % 9;
 
-							if (j < 8) {
+							boolean valid = validBoard();
+							if (valid) {
 								j++;
+								i = (i + j / 9 + 9) % 9;
+								j = (j + 9) % 9;
 							} else {
-								i++;
-								j = 0;
+								StdOut.println("Makes board invalid");
+								board[i][j] = 0;
 							}
+							visBoard();
+
 						} catch (Exception e) {
 							StdOut.println("Invalid input");
 						}
 				}
-				i = (i + 9) % 9;
-				j = (j + 9) % 9;
 			}
 		}
 
